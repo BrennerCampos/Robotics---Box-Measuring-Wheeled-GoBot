07589ddd6fe688245d33b9aaac55684240c093c2
huybeme <huyftw@aim.com>
2021-03-17 21:34:44 -0400

    update

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 var (
 	fwdLoopCounter int // used to keep track how many times we circle around the box
 	tally          int // used to count increments of wheel rotation for measuring distance
-	errCounter     int
+	errCounter     int // used to count offset measurement
 	fwdErr         int
 )
 
@@ -35,23 +35,23 @@ func main() {
 	}
 
 	// crux of gobot framework, factory function to create a new robot
-	robot := gobot.NewRobot("gopigo3sensorChecker", // robot name
+	shelly := gobot.NewRobot("gopigo3sensorChecker", // robot name
 		[]gobot.Connection{raspiAdapter},     // slice of connected robots
 		[]gobot.Device{gopigo3, lidarSensor}, // slice of sensors or actuators for robot
 		mainRobotFunc)                        // variable that holds the robots function
 
-	robot.Start() //run robot function
+	shelly.Start() //run robot function
 }
 
-func turnLeft(gpg *g.Driver) {
-	gpg.SetMotorDps(g.MOTOR_LEFT, 35)
-	gpg.SetMotorDps(g.MOTOR_RIGHT, 70)
-}
-
-func turnRight(gpg *g.Driver) {
-	gpg.SetMotorDps(g.MOTOR_LEFT, 70)
-	gpg.SetMotorDps(g.MOTOR_RIGHT, 35)
-}
+//func turnLeft(gpg *g.Driver) {
+//	gpg.SetMotorDps(g.MOTOR_LEFT, 35)
+//	gpg.SetMotorDps(g.MOTOR_RIGHT, 70)
+//}
+//
+//func turnRight(gpg *g.Driver) {
+//	gpg.SetMotorDps(g.MOTOR_LEFT, 70)
+//	gpg.SetMotorDps(g.MOTOR_RIGHT, 35)
+//}
 
 func moveForward(gpg *g.Driver) {
 	gpg.SetMotorDps(g.MOTOR_LEFT, 70)
@@ -66,8 +66,8 @@ func stopMove(gpg *g.Driver) {
 func takeTurn(gpg *g.Driver) {
 	counter := 3 // *********** should be 3
 
-	fmt.Println("90 degree turn in: ")
-	for counter > 0 {
+	fmt.Println("90 degree turn in: ") // control how far passed the box we go before turning
+	for counter > 0 {                  //		ultimately results in how far we are from box after turning
 		time.Sleep(time.Second)
 		fmt.Println(counter)
 		counter--
@@ -84,7 +84,7 @@ func takeTurn(gpg *g.Driver) {
 	counter = 0
 	for counter < 1 {
 		time.Sleep(time.Second)
-		fmt.Println("extra delay")
+		fmt.Println("extra delay") // could be moved to forwardLoop
 		counter++
 	}
 	fmt.Println()
@@ -96,9 +96,9 @@ func forwardLoop(lidarErr int, gpg *g.Driver) {
 
 	counter := 2
 
-	if lidarErr > 0 {
-		counter = counter + lidarErr
-	}
+	if lidarErr > 0 { // part of unfinished code in main robot loop
+		counter = counter + lidarErr // main purpose is P control - corrects for if box is a certain distance
+	} //	from box from initial state
 
 	fmt.Println("forward loop end in: ")
 	for counter > 0 {
@@ -117,12 +117,12 @@ func robotRunLoop(lidarSensor *i2c.LIDARLiteDriver, gpg *g.Driver) {
 
 	gpg.SetLED(3, 0, 0, 255) // light on - blue (led 4 might be under chip, don't know where led 1 and 2 is or if it exists)
 	count := 0
-	rawDim := [2]float64{0, 0}
+	rawDim := [2]float64{0, 0} // raw values
 
-	errorDim := [2]float64{0, 0}
+	errorDim := [2]float64{0, 0} // used to collect error of measurement
 	fwdErr = 0
 
-	correctDim := [2]float64{0, 0}
+	correctDim := [2]float64{0, 0} // raw values - error = corrected dimensions
 
 	err := lidarSensor.Start()
 	lidarVal, err := lidarSensor.Distance()
@@ -130,8 +130,8 @@ func robotRunLoop(lidarSensor *i2c.LIDARLiteDriver, gpg *g.Driver) {
 		fmt.Errorf("lidar sensor reading error %+v", err)
 	}
 
+	// if box is not in initial view and behind it
 	if lidarVal >= 70 {
-
 		for {
 			err := lidarSensor.Start()
 			lidarVal, err = lidarSensor.Distance()
@@ -145,14 +145,10 @@ func robotRunLoop(lidarSensor *i2c.LIDARLiteDriver, gpg *g.Driver) {
 				break
 			}
 		}
-	}
+	} // end of if
 
 	for { // for(ever) loop to keep robot running
-
-		battery, err := gpg.GetBatteryVoltage()
-		if err != nil {
-			fmt.Errorf("Unable to get battery voltage %+v", err)
-		}
+		count++
 
 		// values of sensors
 		err = lidarSensor.Start()
@@ -160,18 +156,26 @@ func robotRunLoop(lidarSensor *i2c.LIDARLiteDriver, gpg *g.Driver) {
 		if err != nil {
 			fmt.Errorf("lidar sensor reading error %+v", err)
 		}
-		count++
 
+		battery, err := gpg.GetBatteryVoltage()
+		if err != nil {
+			fmt.Errorf("Unable to get battery voltage %+v", err)
+		}
+
+		// returns point of degree of the wheel
 		leftMotor, err := gpg.GetMotorEncoder(g.MOTOR_LEFT)
 		if err != nil {
 			fmt.Errorf("left motor encorder not reading %+v", err)
 		}
 
 		// start tallying degree rotations for measurement
+		// 		based on %3 24 - 44 cm  =  ~ 20 cm = 1.67 mm per tally
+		// 		based on %2 25 - 47 cm = ~ 20 cm = 1.11 cm per tally
+		//		based on %5 16 cm = ~ 2.2 mm per tally
 		if fwdLoopCounter == 1 || fwdLoopCounter == 2 {
 			if leftMotor%2 == 0 {
 				tally++
-
+				// when lidar is offset, start counting for measurement conversion
 				if lidarVal <= 3 || lidarVal > 15 {
 					errCounter++
 				}
@@ -197,7 +201,7 @@ func robotRunLoop(lidarSensor *i2c.LIDARLiteDriver, gpg *g.Driver) {
 		fmt.Printf("|%-20s:   %-4d|\n", "tally", tally)
 		fmt.Printf("|%-20s:   %-4d|\n", "errCounter", errCounter)
 
-		// proportional control
+		// proportional control - corrects for offset readings - maybe should have shifted us back into line instead
 		currentError := 0
 		if lidarVal < 13 { // if under 13, we are in our sweet spot, don't adjust
 			currentError = 0
@@ -205,11 +209,10 @@ func robotRunLoop(lidarSensor *i2c.LIDARLiteDriver, gpg *g.Driver) {
 			currentError = 30
 		} else if lidarVal >= 20 && lidarVal < 70 {
 			currentError = 75
-		} else if lidarVal >= 70 { // if the lidar is no longer "seeing the box"
-
 		}
 		time.Sleep(time.Millisecond * (time.Duration(100 + currentError)))
 
+		// unfinished - need to increment based on distance of the lidar value to control forwardLoop - P control
 		if lidarVal >= 20 || lidarVal < 70 {
 			fwdErr = 1 // fwdErr = 1 @ 10 cm; every 2 cm + another 1 just about
 		}
@@ -218,10 +221,9 @@ func robotRunLoop(lidarSensor *i2c.LIDARLiteDriver, gpg *g.Driver) {
 		if lidarVal >= 70 {
 			fmt.Println("entering turning loop")
 			takeTurn(gpg)
-
 			fmt.Println("entering forward loop")
 			forwardLoop(fwdErr, gpg)
-		} else if lidarVal >= 10 && lidarVal < 60 {
+		} else if lidarVal >= 3 && lidarVal < 70 {
 			moveForward(gpg)
 		}
 
@@ -234,48 +236,35 @@ func robotRunLoop(lidarSensor *i2c.LIDARLiteDriver, gpg *g.Driver) {
 			gpg.SetLED(3, 255, 0, 0) // red
 		}
 
-		// 24 - 44 cm based on %3 = 120 tallys = 1.67 mm per tally
-
-		// based on %2 25 - 47 cm
-		// 20 - 40 = 180 tallys = 1.11 mm per tally
-
-		// 360 degrees = ~ 150 - 170mm = 72 tallys ( based on %5)
-		//		2.2 mm per tally
-
 		if fwdLoopCounter == 1 {
 			rawDim[0] = float64(tally) * 1.11
 
-			if lidarVal <= 3 || lidarVal > 15 {
+			if errCounter == 0 { // if no error, do not allow division by 0
+				errorDim[0] = 0
+			} else {
+				errorDim[0] = float64(errCounter) * 1.11 / float64(errCounter)
+			}
 
-				if errCounter == 0 {
-					errorDim[0] = 0
-				} else {
-					errorDim[0] = float64(errCounter) * 1.11 / float64(errCounter)
-				}
-
-				if lidarVal <= 3 {
-					correctDim[0] = rawDim[0] - errorDim[0]
-				} else if lidarVal > 15 {
-					correctDim[0] = rawDim[0] + errorDim[0]
-				}
+			// correct for error if shelly is not parallel to box
+			if lidarVal <= 3 { // towards box
+				correctDim[0] = rawDim[0] - errorDim[0]
+			} else if lidarVal > 15 { // drifting away
+				correctDim[0] = rawDim[0] + errorDim[0]
 			}
 
 		} else if fwdLoopCounter == 2 {
 			rawDim[1] = float64(tally) * 1.11
 
-			if lidarVal <= 3 || lidarVal > 15 {
+			if errCounter == 0 {
+				errorDim[1] = 0
+			} else {
+				errorDim[1] = float64(errCounter) * 1.11 / float64(errCounter)
+			}
 
-				if errCounter == 0 {
-					errorDim[1] = 0
-				} else {
-					errorDim[1] = float64(errCounter) * 1.11 / float64(errCounter)
-				}
-
-				if lidarVal <= 3 {
-					correctDim[1] = rawDim[1] - errorDim[1]
-				} else if lidarVal > 15 {
-					correctDim[1] = rawDim[1] + errorDim[1]
-				}
+			if lidarVal <= 3 {
+				correctDim[1] = rawDim[1] - errorDim[1]
+			} else if lidarVal > 15 {
+				correctDim[1] = rawDim[1] + errorDim[1]
 			}
 
 		} else if fwdLoopCounter >= 3 && fwdLoopCounter < 5 {
